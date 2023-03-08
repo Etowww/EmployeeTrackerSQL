76d52d93b62ebc7004dde8a373ea78b78dc77e8b
Etowww <etow.engineering@gmail.com>
2023-03-07 21:04:51 -0700

    Populated the query.js with all the necessary query methods

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 // Connect to the database
-const connection = mysql.createconnection(
+const connection = mysql.createConnection(
     {
         host: '127.0.0.1',
         user: 'root',
--- db/query.js
+++ db/query.js
@@ -6,14 +6,53 @@ class Information {
         this.connection = connection;
     };
 
+    async viewAllDepartments() {
+        const query = 'SELECT * FROM department';
+        const [rows] = await this.connection.promise().query(query);
+        return rows.map(({dept_id, name}) => ({id: dept_id, name}));
+    };
 
-    viewDepartments () {
-        return this.connection.promise().query()
+
+    viewAllRoles () {
+        const query = 'SELECT * FROM roles';
+        return this.connection.query(query);
     }
 
-    insertEmployee(a, b, c, d){
-        return this.connection.promise().query(`INSERT INTO employee (first_name, last_name, ) VALUES ( ${a}, "Linear equations, inequalities, functions, and graphs", true)`) 
+    viewAllEmployees () {
+        const query = `SELECT employee.id AS 'Employee ID',
+        Employee.first_name AS 'First Name',
+        Employee.last_name AS 'Last Name',
+        role.title AS 'Title',
+        department.name AS 'Department',
+        role.salary AS 'Salary',
+        CONCAT(manager.first_name, ' ', manager.last_name) AS 'Manager'
+        FROM employee
+        LEFT JOIN role ON employee.role_id = role.id
+        LEFT JOIN department ON role.department_id = department.id
+        LEFT JOIN employee manager ON employee.manager_id = manager.id
+        `;
+        return this.connection.query(query);
+    }
+
+    addDepartment(department) {
+        const query = 'INSERT INTO department SET ?';
+        return this.connection.query(query, role);
+    }
+
+    addRole(role) {
+        const query = 'INSERT INTO roles SET ?';
+        return this.connection.query(query, role);
+    }
+
+    addEmployee(employee) {
+        const query = 'INSERT INTO employee SET ?';
+        return this.connection.query(query, role);
+    }
+
+    updateEmployeeRole(employeeId, roleID) {
+        const query = 'UPDATE employee SET role_id = ? WHERE ID = ?'
+        return this.connection.query(query, [roleID, employeeId])
     }
 }
 
-module.exports = new Information(connection)
+module.exports = Information;
