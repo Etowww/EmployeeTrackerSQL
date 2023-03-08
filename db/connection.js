7c212479cace19434037e92e87f963ddac7b4b09
Etowww <etow.engineering@gmail.com>
2023-03-07 17:06:15 -0700

    File restructure / new files added

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+// Connect to the database
+const connection = mysql.createconnection(
+    {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'password',
+        database: 'company_db'
+    },
+    console.log(`Connected to the company_db`)
+);
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,14 +1,92 @@
 // Import and Require NPM Packages
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const db = require('./db/query')
 
-// Connect to the database
-const connection = mysql.createconnection(
+const menuQuestions = [
     {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'password',
-        database: 'company_db'
-    },
-    console.log(`Connected to the company_db`)
-);
+        message: 'What would you like to do?',
+        type: 'list',
+        name: 'userChoice',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add Department',
+            'Add A Role',
+            'Add An Employee',
+            'Update an Employee Role',
+            'exit'
+        ],
+    }
+];
+
+handleMenuQuestions()
+
+function handleMenuQuestions() {
+    inquirer.prompt(menuQuestions)
+        .then(answers => {
+            switch (answers.userChoice) {
+                case 'View All Departments':
+                    viewAllDepartments();
+                    break;
+                // case 'View All Departments':
+
+                // break;
+                // case 'View All Departments':
+
+                // break;
+                // case 'View All Departments':
+
+                // break;
+                // case 'View All Departments':
+
+                // break;
+                // case 'View All Departments':
+
+                // break;
+                // case 'View All Departments':
+
+                // break;
+                default:
+                    process.exit();
+            }
+        })
+
+}
+
+function viewAllDepartments() {
+    db.viewDepartments().then((data) => {
+        console.table(data)
+        handleMenuQuestions()
+    })
+   
+}
+
+function addEmployee(){
+    inquirer.prompt([
+
+        {
+            message: 'What is the engineers name?',
+            type: 'input',
+            name: 'first_name',
+        },
+        {
+            message: 'What is the engineers ID?',
+            type: 'input',
+            name: 'last_name',
+        },
+        {
+            message: 'What is the engineers Email Address?',
+            type: 'input',
+            name: 'role_id',
+        },
+        {
+            message: 'What is the engineers Github Account?',
+            type: 'input',
+            name: 'manager_id',
+        }
+    ])
+    .then(answers => {
+       db.insertEmployee(answers.first_name, answers.last_name, )
+    })
+}
