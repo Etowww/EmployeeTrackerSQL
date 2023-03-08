e6f2a71a0bfd5f94cfb470aed82b66e0164cb686
Etowww <etow.engineering@gmail.com>
2023-03-07 22:28:13 -0700

    Changed the mySQL to be promised base / changed query syntax / added three functions to the index.js page

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Connect to the database
-const connection = mysql.createConnection(
+const connection = async () => {
+    const db = await mysql.createConnection(
     {
         host: '127.0.0.1',
         user: 'root',
@@ -9,6 +10,7 @@ const connection = mysql.createConnection(
         database: 'company_db'
     },
     console.log(`Connected to the company_db`)
-);
-
+    );
+    return db
+};
 module.exports = connection;
--- db/query.js
+++ db/query.js
@@ -7,52 +7,40 @@ class Information {
     };
 
     async viewAllDepartments() {
-        const query = 'SELECT * FROM department';
-        const [rows] = await this.connection.promise().query(query);
-        return rows.map(({dept_id, name}) => ({id: dept_id, name}));
+        const query = await connection();
+        const [rows, fields] = await query.execute('SELECT * FROM department');
+        return rows;
     };
 
 
-    viewAllRoles () {
-        const query = 'SELECT * FROM roles';
-        return this.connection.query(query);
+    async viewAllRoles () {
+        const query = await connection();
+        const [rows, fields] = await query.execute('SELECT * FROM roles');
+        return rows;
     }
 
-    viewAllEmployees () {
-        const query = `SELECT employee.id AS 'Employee ID',
-        Employee.first_name AS 'First Name',
-        Employee.last_name AS 'Last Name',
-        role.title AS 'Title',
-        department.name AS 'Department',
-        role.salary AS 'Salary',
-        CONCAT(manager.first_name, ' ', manager.last_name) AS 'Manager'
-        FROM employee
-        LEFT JOIN role ON employee.role_id = role.id
-        LEFT JOIN department ON role.department_id = department.id
-        LEFT JOIN employee manager ON employee.manager_id = manager.id
-        `;
-        return this.connection.query(query);
-    }
+    async viewAllEmployees () {
+        const query = await connection();
+        const [rows, fields] = await query.execute(`
+            SELECT
+                employee.employee_id,
+                employee.first_name,
+                employee.last_name,
+                roles.role_title AS role,
+                department.name AS department,
+                roles.role_salary,
+                CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+            FROM
+                employee
+                LEFT JOIN roles ON employee.employee_role_id = roles.role_id
+                LEFT JOIN department ON roles.role_dept_id = department.dept_id
+                LEFT JOIN employee manager ON employee.manager_id = manager.employee_id;
+        `);
+        return rows;
+    };
 
-    addDepartment(department) {
-        const query = 'INSERT INTO department SET ?';
-        return this.connection.query(query, role);
-    }
 
-    addRole(role) {
-        const query = 'INSERT INTO roles SET ?';
-        return this.connection.query(query, role);
-    }
 
-    addEmployee(employee) {
-        const query = 'INSERT INTO employee SET ?';
-        return this.connection.query(query, role);
-    }
-
-    updateEmployeeRole(employeeId, roleID) {
-        const query = 'UPDATE employee SET role_id = ? WHERE ID = ?'
-        return this.connection.query(query, [roleID, employeeId])
-    }
 }
 
 module.exports = Information;
